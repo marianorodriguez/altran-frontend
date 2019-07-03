dbf26343fe619e9672895b8cc33984e60d5f7d86
marianorodriguez <mariano.rodriguez.232@gmail.com>
2019-07-03 00:11:03 -0300

    searchbar design

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -15,7 +15,25 @@ class Searchbar extends Component {
   }
 
   render() {
-    return (<input onChange={this.handleInputChange} />);
+    return (
+      <section className="hero is-primary is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
+              Gnome finder
+            </h1>
+            <h2 className="subtitle">
+              Find your gnome.
+            </h2>
+            <div className="field">
+              <div className="control">
+                <input className="input is-primary" type="text" placeholder="search by name or profession" onChange={this.handleInputChange} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 
