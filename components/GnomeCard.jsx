3b83aa6ade275ea2eac02ad4e90578f858c43e33
marianorodriguez <mariano.rodriguez.232@gmail.com>
2019-07-03 17:44:31 -0300

    gnome card style

--- components/GnomeCard.jsx
+++ components/GnomeCard.jsx
@@ -4,10 +4,23 @@ import PropTypes from 'prop-types';
 
 const GnomeCard = ({ gnome }) => (
   <div className="tile is-parent">
-    <div className="tile is-child box card">{gnome.name}</div>
+    <div className="tile is-child box card gnome-card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={gnome.thumbnail} alt="" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{gnome.name}</p>
+            <p className="subtitle is-6">{gnome.professions.join(', ')}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
-
 
 GnomeCard.propTypes = {
   gnome: PropTypes.objectOf(PropTypes.any).isRequired,
