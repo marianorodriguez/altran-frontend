ecf913d38105f31148bb4f075369c3d5b80535eb
marianorodriguez <mariano.rodriguez.232@gmail.com>
2019-07-03 00:29:27 -0300

    load more check

--- components/LoadMore.jsx
+++ components/LoadMore.jsx
@@ -31,14 +31,25 @@ class LoadMore extends Component {
   }
 
   render() {
-    return (
-      <div className="centered button is-outlined is-primary" onClick={this.loadMoreGnomes}>LOAD MORE</div>
-    );
+    const { loading, total } = this.props;
+    if (!loading && total > 0) {
+      return (
+        <div className="centered button is-outlined is-primary" onClick={this.loadMoreGnomes}>LOAD MORE</div>
+      );
+    }
+    return <div />;
   }
 }
 
 LoadMore.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  total: PropTypes.number.isRequired,
   dispatch: PropTypes.func.isRequired,
 };
 
-export default connect(() => ({}))(LoadMore);
+const mapStateToProps = state => ({
+  loading: state.gnomes.loading,
+  total: state.gnomes.items.length,
+});
+
+export default connect(mapStateToProps)(LoadMore);
